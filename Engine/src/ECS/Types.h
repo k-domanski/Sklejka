abcb024d209949b2c52d39e8a8ca6a40dc303aa0
LinearJester <linearjester@gmail.com>
2021-03-30 23:48:32 +0200

    types fix

--- Engine/src/ECS/Types.h
+++ Engine/src/ECS/Types.h
@@ -18,11 +18,15 @@ namespace ECS {
   static const ComponentTypeID GetComponentTypeID() {
     static_assert((std::is_base_of< Component, T >::value && !std::is_same< Component, T >::value),
                   "INVALID TEMPLATE TYPE");
+    static const ComponentTypeID typeID = GetRuntimeComponentTypeID();
+    return typeID;
   }
 
   template< typename T >
   static const ComponentTypeID GetSystemTypeID() {
     static_assert((std::is_base_of< System, T >::value && !std::is_same< System, T >::value),
                   "INVALID TEMPLATE TYPE");
+    static const SystemTypeID typeID = GetRuntimeSystemTypeID();
+    return typeID;
   }
 }  // namespace ECS
