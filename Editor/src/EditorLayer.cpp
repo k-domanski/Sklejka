f542b3a08a3dcbabdd868bb6b9a8a6daba110462
k-dom <224285@edu.p.lodz.pl>
2021-04-19 12:55:44 +0200

    fix merge

--- Editor/src/EditorLayer.cpp
+++ Editor/src/EditorLayer.cpp
@@ -11,12 +11,12 @@ void EditorLayer::OnAttach() {
   auto coneModel = AssetManager::GetModel("./models/smolCone.fbx");
   m_Shader       = AssetManager::GetShader("./shaders/default.glsl");
   assert(("Failed to acquire shader", m_Shader != nullptr));
-  m_ConeMesh        = coneModel->getRootMesh();
-  //m_PepeModel       = AssetManager::GetModel("./models/squirrel.fbx");
-  m_PepeModel       = AssetManager::GetModel("./models/silly_dancing.fbx");
-  //auto tex_shader   = AssetManager::GetShader("./shaders/texture_shader.glsl");
+  m_ConeMesh = coneModel->getRootMesh();
+  // m_PepeModel       = AssetManager::GetModel("./models/squirrel.fbx");
+  m_PepeModel = AssetManager::GetModel("./models/silly_dancing.fbx");
+  // auto tex_shader   = AssetManager::GetShader("./shaders/texture_shader.glsl");
   auto pepe_texture = AssetManager::GetTexture2D("./textures/Stormtrooper_D.png");
-  //auto pepe_texture = AssetManager::GetTexture2D("./textures/Untilted.png");
+  // auto pepe_texture = AssetManager::GetTexture2D("./textures/Untilted.png");
   m_PepeMaterial = AssetManager::GetMaterial(m_Shader, "pepe_tex", pepe_texture);
 
   auto aspect        = Engine::Window::Get().GetAspectRatio();
@@ -24,7 +24,7 @@ void EditorLayer::OnAttach() {
   m_EditorCamera.camera =
       camera_entity->AddComponent< Engine::Camera >(45.0f, aspect, 0.001f, 1000.0f);
   m_EditorCamera.transform = camera_entity->AddComponent< Engine::Transform >();
-  m_Scene.SceneGraph()->AddEntity(camera_entity->GetID());
+  m_Scene->SceneGraph()->AddEntity(camera_entity->GetID());
 
   /*Camera*/
   m_EditorCamera.camera->flags.Set(Engine::CameraFlag::MainCamera);
@@ -38,11 +38,10 @@ void EditorLayer::OnAttach() {
   m_Entity2  = ECS::EntityManager::GetInstance().CreateEntity();
   m_Pepe     = ECS::EntityManager::GetInstance().CreateEntity();
   m_Material = serializer->LoadMaterial("./material.json");
-  //m_Material = AssetManager::GetMaterial(m_Shader, "./shaders/default.glsl",
+  // m_Material = AssetManager::GetMaterial(m_Shader, "./shaders/default.glsl",
   //                                       "./textures/pepo_sad.png", texture);
 
-
-  //serializer->SaveMaterial(m_Material, "./material.json");
+  // serializer->SaveMaterial(m_Material, "./material.json");
 
   m_Entity1->AddComponent< Transform >();
   m_Entity1->AddComponent< Components::MeshRenderer >(m_ConeMesh, m_Material);
@@ -50,10 +49,27 @@ void EditorLayer::OnAttach() {
   m_Entity2->AddComponent< Components::MeshRenderer >(m_ConeMesh, m_Material);
   m_PepeTransform = m_Pepe->AddComponent< Transform >();
   m_Pepe->AddComponent< Components::MeshRenderer >(m_PepeModel->getRootMesh(), m_PepeMaterial);
-  auto sg = m_Scene.SceneGraph();
+  auto sg = m_Scene->SceneGraph();
   auto id = m_Entity1->GetID();
   sg->AddEntity(id);
   // m_Scene.SceneGraph()->AddEntity(m_Entity2->GetID(), m_Entity1->GetID());
+
+  /*SceneHierarchyPanel Test*/
+  m_SceneHierarchyPanel.SetScene(m_Scene);
+  /*auto ent1 = ECS::EntityManager::GetInstance().CreateEntity();
+  auto ent2 = ECS::EntityManager::GetInstance().CreateEntity();
+  auto ent3 = ECS::EntityManager::GetInstance().CreateEntity();
+  auto ent4 = ECS::EntityManager::GetInstance().CreateEntity();
+  ent1->AddComponent< Transform >();
+  ent2->AddComponent< Transform >();
+  ent3->AddComponent< Transform >();
+  ent4->AddComponent< Transform >();
+
+  sg->AddEntity(ent1->GetID());
+  sg->AddEntity(ent2->GetID(), ent1->GetID());
+  sg->AddEntity(ent3->GetID(), ent1->GetID());
+  sg->AddEntity(ent4->GetID(), ent1->GetID());*/
+  /*------------------------*/
 
   auto box1 = m_Entity1->AddComponent< Components::BoxCollider >();
   auto rb1  = m_Entity1->AddComponent< Components::Rigidbody >();
@@ -86,18 +102,18 @@ void EditorLayer::OnUpdate(double deltaTime) {
   /* TODO: CameraController move to separate class?*/
   UpdateEditorCamera();
   /* -------------------------- */
-   m_Time += deltaTime / 2.0f;
+  m_Time += deltaTime / 2.0f;
   auto tr1 = m_Entity1->GetComponent< Transform >();
   // tr1->Rotate(deltaTime * 0.3, {0.0f, 1.0f, 0.0f});
   // m_PepeTransform->Rotate(deltaTime * 0.1, {0.0f, 1.0f, 0.0f});
-  //auto pos = tr1->Position();
+  // auto pos = tr1->Position();
   tr1->Position(glm::vec3(sin(m_Time) * m_it, 0.0f, 0.0f));
   m_it += deltaTime / 20.0f;
-  //std::cout << "sin time: " << sin(m_Time)*m_it << std::endl;
+  // std::cout << "sin time: " << sin(m_Time)*m_it << std::endl;
 
-  m_Scene.Update(deltaTime);
+  m_Scene->Update(deltaTime);
   /*Update systemów w aplikacji?*/
-  m_Scene.Draw();
+  m_Scene->Draw();
 }
 
 void EditorLayer::OnDetach() {
@@ -108,6 +124,11 @@ void EditorLayer::OnEvent(Event& event) {
   dispatcher.Dispatch< MouseScrolledEvent >(BIND_EVENT_FN(EditorLayer::OnMouseScroll));
   dispatcher.Dispatch< MouseButtonPressedEvent >(BIND_EVENT_FN(EditorLayer::OnMouseButtonPress));
   dispatcher.Dispatch< MouseButtonReleasedEvent >(BIND_EVENT_FN(EditorLayer::OnMouseButtonRelease));
+}
+
+void EditorLayer::OnImGuiRender() {
+  m_SceneHierarchyPanel.OnImGuiRender();
+  m_InspectorPanel.OnImGuiRender(m_SceneHierarchyPanel.GetSelectedEntity());
 }
 
 bool EditorLayer::OnMouseScroll(MouseScrolledEvent& e) {
