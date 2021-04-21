5be62fa9b3aa630ec55325ecd346015fe84a6696
k-dom <224285@edu.p.lodz.pl>
2021-04-21 16:27:01 +0200

    editor camera not showing in scene hierarchy, cannot add/modify root components.

--- Editor/src/EditorLayer.cpp
+++ Editor/src/EditorLayer.cpp
@@ -32,7 +32,8 @@ void EditorLayer::OnAttach() {
   m_Scene->SceneGraph()->AddChild(0, camera_entity->GetID());
 
   /*Camera*/
-  m_EditorCamera.camera->flags.Set(Engine::CameraFlag::MainCamera);
+  // m_EditorCamera.camera->flags.Set(Engine::CameraFlag::MainCamera);
+  m_EditorCamera.camera->flags.Set(Engine::CameraFlag::EditorCamera);
   editorCameraArgs.screenSize = Window::Get().GetScreenSize();
   m_EditorCamera.transform->Position({0.0f, 0.0f, 2.0f});
   m_EditorCamera.transform->Rotate(glm::radians(180.0f), {0.0f, 1.0f, 0.0f});
