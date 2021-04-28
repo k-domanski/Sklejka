538708afc3df8819404821bf0ea4d0ec7a02a582
Lukapl666 <wojciech.piatkowski.dev@gmail.com>
2021-04-28 12:32:18 +0200

    light fix

--- Engine/src/GL/UniformBufferData.h
+++ Engine/src/GL/UniformBufferData.h
@@ -13,10 +13,33 @@ namespace Engine::GL {
     glm::mat4 viewProjection;  // 16 byte alignment
   };
   struct DirectionalLightUniformData {
-    glm::vec3 direction;  // 16 byte alignment
-    glm::vec3 ambient;    // 16 byte alignment
-    glm::vec3 diffuse;    // 16 byte alignment
-    glm::vec3 specular;   // 16 byte alignment
-    float intensity;      // 16 byte alignment
+    /*
+      On OpenGL side vec3 and vec4 have 16 byte alignment.
+      This means that data for these types have to begin at addresses (offsets)
+        that are a multiple of 16: 0x0, 0x16, 0x32 ...
+      On C++ side, vec3 is 3 * float (4 bytes) = 12 bytes.
+      So consecutive vec3s have addresses (offsets): 0x0, 0x12, 0x24, 0x32...
+      We have to add padding in the struct
+    */
+    glm::vec3 direction;  // Data 12 bytes
+    float __pad1[1]{0};   // Padding 4 bytes
+                          // Total 16 bytes
+
+    glm::vec3 ambient;   // Data 12 bytes
+    float __pad2[1]{0};  // Padding 4 byte
+                         // Total 16 bytes
+
+    glm::vec3 diffuse;   // Data 12 bytes
+    float __pad3[1]{0};  // Padding 4 bytes
+                         // Total 16 bytes
+
+    glm::vec3 specular;  // Data 12 bytes
+    /*
+      Now for the intensity: 1 float has alighment of 4 bytes and has to be on addresses
+        that are multipleof 4: 0x0, 0x4, 0x8, ...
+      Since previous data type was vec3 witch has 12 bytes,
+        the intensity is already aligned - no padding.
+    */
+    float intensity;  // Data 4 bytes
   };
 }  // namespace Engine::GL
--- Engine/src/Systems/LightSystem.cpp
+++ Engine/src/Systems/LightSystem.cpp
@@ -3,33 +3,27 @@
 #include <ECS/EntityManager.h>
 
 namespace Engine::Systems {
-	using ECS::EntityManager;
-	LightSystem::LightSystem(): m_LightUniformSlot(2u), m_Light(nullptr)
-	{
-		AddSignature<DirectionalLight>();
-	}
-	auto LightSystem::Update(float deltaTime) -> void
-	{
-		if (m_Light == nullptr)
-		{
-			for (auto entityID : _entities) {
-				auto light = EntityManager::GetComponent<DirectionalLight>(entityID);
-				m_Light = light;
-			}
-		}
+  using ECS::EntityManager;
+  LightSystem::LightSystem(): m_LightUniformSlot(2u), m_Light(nullptr) {
+    AddSignature< DirectionalLight >();
+  }
+  auto LightSystem::Update(float deltaTime) -> void {
+    if (m_Light == nullptr) {
+      for (auto entityID : _entities) {
+        auto light = EntityManager::GetComponent< DirectionalLight >(entityID);
+        m_Light    = light;
+      }
+    }
 
-		if (m_Light != nullptr)
-		{
-		
-		if (m_Light->flags.Get(LightFlag::NewData)) {
-			m_LightUniformBuffer.SetData(m_Light->UniformData());
-		}
+    if (m_Light != nullptr) {
+      if (m_Light->flags.Get(LightFlag::NewData)) {
+        m_LightUniformBuffer.SetData(m_Light->UniformData());
+      }
 
-		m_LightUniformBuffer.BindToSlot(m_LightUniformSlot);
-		}
-	}
-	auto LightSystem::Light() const noexcept -> std::shared_ptr<DirectionalLight>
-	{
-		return m_Light;
-	}
-}
+      m_LightUniformBuffer.BindToSlot(m_LightUniformSlot);
+    }
+  }
+  auto LightSystem::Light() const noexcept -> std::shared_ptr< DirectionalLight > {
+    return m_Light;
+  }
+}  // namespace Engine::Systems
