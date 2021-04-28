8e463f7d3c455e56499321eafbfb6702148f9432
k-dom <224285@edu.p.lodz.pl>
2021-04-29 00:48:52 +0200

    created new scene, moved light uniform and function to include, color shader with lightning.

--- Editor/shaders/color_light.glsl
+++ Editor/shaders/color_light.glsl
@@ -3,15 +3,10 @@
 #shader vertex
 #include "vs_common.incl"
 
-// uniform mat4 mvp;
-// uniform mat4 u_model_matrix;
-
 out vec3 v_pos;
 out vec2 v_uv;
 out vec3 v_normal;
-
 void main() {
-  v_uv = a_UV;
   v_normal = mat3(transpose(inverse(u_Model))) * a_Normal;
   gl_Position = u_ModelViewProjection * vec4(a_Position, 1.0f);
   v_pos       = gl_Position.xyz / gl_Position.w;
@@ -22,15 +17,13 @@ void main() {
 #include "fs_common.incl"
 #include "include/fs_light.incl"
 
-
 in vec3 v_pos;
 in vec2 v_uv;
 in vec3 v_normal;
 out vec4 out_color;
+
 void main() {
-	/*view direction for specular*/
-	//vec3 viewDirection = normalize(cameraPosition, v_pos);
-	vec3 result = calculateDirectionalLight(v_normal, v_uv);
+    vec3 result = calculateDirectionalLightColor(v_normal, u_MainColor);
     out_color = vec4(result, 1.0f);
 }
 #endshader
